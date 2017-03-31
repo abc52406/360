28a3289c1d7b4f04ec39733d89091be53744d883
Bob <pb128@hotmail.com>
2017-03-31 14:27:27 +0800

    继续修正

--- js/photo-sphere-viewer.js
+++ js/photo-sphere-viewer.js
@@ -777,7 +777,7 @@ var PhotoSphereViewer = function(args) {
 				var diff = d - touchzoom_dist;
 
 				if (diff != 0) {
-					var direction = diff / Math.abs(diff)*2;
+					var direction = diff / Math.abs(diff)*1.2;
 					zoom(zoom_lvl + direction);
 
 					touchzoom_dist = d;
