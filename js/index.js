34ca6fcc90d8ba11bb704f2ee593abac1bb123c1
abc52406 <286933946@qq.com>
2017-03-31 09:30:43 +0800

    优化了分享逻辑的代码加载

--- js/index.js
+++ js/index.js
@@ -3,7 +3,15 @@ $(function () {
 	var imgpath=GetQueryString("ip")||'sun.jpg';
 	var title=decodeURI(GetQueryString("tt")||'全景图');
 	$(document).attr("title",title);
-	loadPredefinedPanorama();
+    loadPredefinedPanorama();
+
+    if (GetQueryString("share") == "T") {
+        loadscript("http://res.wx.qq.com/open/js/jweixin-1.0.0.js", function () {
+            loadscript("http://qiniu.mgcc.com.cn/wechat/js/base64.min.js", function () {
+                loadscript("js/share.js")
+            })
+        })
+    }
 	function loadPredefinedPanorama() {
 		var div = document.getElementById('container');
 		var PSV = new PhotoSphereViewer({
@@ -34,7 +42,25 @@ $(function () {
 			});
 		};
 		reader.readAsDataURL(file);
-	}
+    }
+
+    function loadscript(url, callback) {
+        var script = document.createElement("script");
+        if (script.readyState) {
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") { script.onreadystatechange = null; callback && callback(); }
+            }
+        }
+        else {
+            script.onload = function () {
+                callback && callback();
+            }
+        }
+        var v = GetQueryString("ver") || "1";
+        script.src = (url.indexOf("?") > 0) ? (url + "&ver=" + v) : (url + "?ver=" + v);
+        var _s = document.getElementsByTagName("script")[0];
+        _s.parentNode.insertBefore(script, _s);
+    }
 });
 function GetQueryString(prop, url) {
     var re = new RegExp("[?|&]" + prop + "=([^\\&]*)", "i");
@@ -52,72 +78,4 @@ function GetQueryString(prop, url) {
     var a = re.exec(url);
     if (a == null) return "";
     return a[1];
-}
-
-var mpid = 'feC-43Ecgh09oDfg-3C';
-var share_pic = GetQueryString("si")||'';
-var share_url;
-var weChatUrlBase =  'http://server.pengbo.info/wxapi/jssdkjsonp?mpid='+ mpid +'&callback=1234&callurl=';
-var weChatUrl;
-var sharetitle=GetQueryString("st")||'全景图';
-var sharedesc=GetQueryString("sd")||'';
-wechatConfig();
-function wechatConfig() {
-    weChatUrl = weChatUrlBase + location.href.replace(/&/g, '@').split('#')[0];
-    share_url = window.location.href;
-    $.ajax({
-        type: 'post',
-        url: weChatUrl,
-        dataType: "jsonp",
-        crossDomain: !0,
-        jsonpCallback: "callbackfn",
-        success: function(data) {
-            var config_obj = data;
-            wx.config({
-                debug: false,
-                appId: config_obj.appId,
-                timestamp: config_obj.timestamp,
-                nonceStr: config_obj.nonceStr,
-                signature: config_obj.signature,
-                jsApiList: [
-                    // 所有要调用的 API 都要加到这个列表中
-                    'onMenuShareTimeline',
-                    'onMenuShareAppMessage'
-                ]
-            });
-            wx.ready(function () {
-                wx.onMenuShareAppMessage({
-                    title: sharetitle,
-                    desc: sharedesc,
-                    link: share_url.replace(/\@/g, '#'),
-                    imgUrl: share_pic,
-                    success: function (res) {
-						_hmt.push(['_trackEvent', 'click', 'sharefriend','','']);
-                    },
-                    cancel: function (res) {
-                    },
-                    fail: function (res) {
-                    }
-                });
-
-                // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
-                wx.onMenuShareTimeline({
-                    title: sharedesc,
-                    link: share_url.replace(/\@/g, '#'),
-                    imgUrl: share_pic,
-                    success: function (res) {
-						_hmt.push(['_trackEvent', 'click', 'sharefriendspace','','']);
-                    },
-                    cancel: function (res) {
-                    },
-                    fail: function (res) {
-                    }
-                });
-            });
-            wx.error(function(res){
-            });
-        },
-        error:function(xhr, type) {
-        }
-    });
 }
